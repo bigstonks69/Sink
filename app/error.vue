40d80ca698aedfee20694e5ed0d23cd5d4c4a4bc
bigstonks69 <danielgrabski@gmail.com>
2026-02-23 17:07:32 +0100

    Modify error.vue to redirect on mount
    Redirects users to a specified URL upon mounting the component.

--- app/error.vue
+++ app/error.vue
@@ -1,16 +1,15 @@
-<script setup lang="ts">
-import errorImage from './assets/images/404.svg?raw'
+<script setup>
+import { onMounted } from 'vue'
+
+const redirectUrl = 'https://linkbudd.com/eiotclub' // <-- Your desired URL
+
+onMounted(() => {
+  window.location.href = redirectUrl
+})
 </script>
 
 <template>
-  <NuxtLayout name="default">
-    <a
-      class="
-        mx-auto flex h-full w-full max-w-[600px] items-center justify-center
-        [&_svg]:w-full
-      "
-      href="/"
-      v-html="errorImage"
-    />
-  </NuxtLayout>
+  <div class="flex items-center justify-center min-h-screen">
+    <p>Redirecting...</p>
+  </div>
 </template>
